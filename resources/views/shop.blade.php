75fe88cc428025fcdf2e5be5a6744855c4038ece
thanhnt411 <darkphoenix4112006@gmail.com>
2025-10-23 14:22:06 +0700

    add filter price

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -182,16 +182,16 @@
                         <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
                             aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
                             <input class="price-range-slider" type="text" name="price_range" value=""
-                                data-slider-min="10" data-slider-max="1000" data-slider-step="5"
-                                data-slider-value="[250,450]" data-currency="$" />
+                                data-slider-min="1" data-slider-max="1000" data-slider-step="5"
+                                data-slider-value="[{{ $min_price }},{{ $max_price }}]" data-currency="$" />
                             <div class="price-range__info d-flex align-items-center mt-2">
                                 <div class="me-auto">
                                     <span class="text-secondary">Min Price: </span>
-                                    <span class="price-range__min">$250</span>
+                                    <span class="price-range__min">$1</span>
                                 </div>
                                 <div>
                                     <span class="text-secondary">Max Price: </span>
-                                    <span class="price-range__max">$450</span>
+                                    <span class="price-range__max">$500</span>
                                 </div>
                             </div>
                         </div>
@@ -471,6 +471,8 @@
         <input type="hidden" name="order" id="order" value="{{ $order }}">
         <input type="hidden" name="brands" id="hdnBrand">
         <input type="hidden" name="categories" id="hdnCategory">
+        <input type="hidden" name="min" id="hdnMinPrice" value="{{ $min_price }}">
+        <input type="hidden" name="max" id="hdnMaxPrice" value="{{ $max_price }}">
     </form>
 @endsection
 
@@ -512,6 +514,14 @@
                 $("#hdnCategory").val(categories);
                 $("#frmfilter").submit();
             });
+
+            $("[name='price_range']").on('change', function() {
+                var min = $(this).val().split(',')[0];
+                var max = $(this).val().split(',')[1];
+                $("#hdnMinPrice").val(min);
+                $("#hdnMaxPrice").val(max);
+                $("#frmfilter").submit();
+            })
         });
     </script>
 @endpush

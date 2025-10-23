0e35f9a817abd04d818738f3780ce6caaaad6105
thanhnt411 <darkphoenix4112006@gmail.com>
2025-10-23 09:46:43 +0700

    add product sorting

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,10 +7,40 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::orderBy('created_at', 'DESC')->paginate(12);
-        return view('shop', compact('products'));
+        $size = $request->query('size') ?  $request->query('size') : 12;
+        $o_colum = '';
+        $o_order = '';
+        $order = $request->query('order') ? $request->query('order') : -1;
+        switch ($order) {
+            case 1:
+                $o_colum = 'created_at';
+                $o_order = 'DESC';
+                break;
+
+            case 2:
+                $o_colum = 'created_at';
+                $o_order = 'ASC';
+                break;
+
+            case 3:
+                $o_colum = 'sale_price';
+                $o_order = 'DESC';
+                break;
+
+            case 4:
+                $o_colum = 'sale_price';
+                $o_order = 'ASC';
+                break;
+
+            default:
+                $o_colum = 'id';
+                $o_order = 'DESC';
+                break;
+        }
+        $products = Product::orderBy($o_colum, $o_order)->paginate($size);
+        return view('shop', compact('products', 'size', 'order'));
     }
 
     public function products_detail($product_slug)
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -366,16 +366,20 @@
                     <div
                         class="shop-acs d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
                         <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
-                            aria-label="Sort Items" name="total-number">
-                            <option selected>Default Sorting</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Best selling</option>
-                            <option value="3">Alphabetically, A-Z</option>
-                            <option value="3">Alphabetically, Z-A</option>
-                            <option value="3">Price, low to high</option>
-                            <option value="3">Price, high to low</option>
-                            <option value="3">Date, old to new</option>
-                            <option value="3">Date, new to old</option>
+                            style="margin-right:20px" aria-label="Page size" id="pagesize" name="pagesize">
+                            <option value="12" {{ $size == 12 ? 'selected' : '' }}>Show</option>
+                            <option value="24" {{ $size == 24 ? 'selected' : '' }}>24</option>
+                            <option value="48" {{ $size == 48 ? 'selected' : '' }}>48</option>
+                            <option value="102" {{ $size == 102 ? 'selected' : '' }}>102</option>
+                        </select>
+
+                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
+                            aria-label="Sort Items" name="sortitem" id="sortitem">
+                            <option value="-1" {{ $order == -1 ? 'selected' : '' }}>Default Sorting</option>
+                            <option value="1" {{ $order == 1 ? 'selected' : '' }}>Date, new to old</option>
+                            <option value="2" {{ $order == 2 ? 'selected' : '' }}>Date, old to new</option>
+                            <option value="3" {{ $order == 3 ? 'selected' : '' }}>Price, high to low</option>
+                            <option value="4" {{ $order == 4 ? 'selected' : '' }}>Price, low to high</option>
                         </select>
 
                         <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -512,9 +516,33 @@
 
                 <div class="divider"></div>
                 <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-                    {{ $products->links('pagination::bootstrap-5') }}
+                    {{ $products->withQueryString()->links('pagination::bootstrap-5') }}
                 </div>
             </div>
         </section>
     </main>
+    <form id="frmfilter" method="GET" action="{{ route('shop.index') }}">
+        @csrf
+        <input type="hidden" name="page" value="{{ $products->currentPage() }}">
+        <input type="hidden" name="size" id="size" value="{{ $size }}">
+        <input type="hidden" name="order" id="order" value="{{ $order }}">
+    </form>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function() {
+            $("#pagesize").on('change', function() {
+                $("#size").val($("#pagesize option:selected").val());
+                $("#frmfilter").submit();
+            })
+        });
+
+        $(function() {
+            $("#sortitem").on('change', function() {
+                $("#order").val($("#sortitem option:selected").val());
+                $("#frmfilter").submit();
+            })
+        });
+    </script>
+@endpush
